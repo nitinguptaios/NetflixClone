c008a8de3789337f2498c6c0b28f1254af6a8681
Lekha-iphtech <nitin.gupta@iphtechnologies.com>
2022-07-16 22:55:39 +0530

    Create Project

---
+++ NetflixClone/Controllers/General/AirplayDemoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AirplayDemoViewController.swift
+//  NetflixClone
+//
+//  Created by Nitin Gupta on 20/05/22.
+//
+
+import UIKit
+
+class AirplayDemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ NetflixClone/Resources/AppConstants.swift
@@ -0,0 +1,13 @@
+//
+//  AppConstants.swift
+//  NetflixClone
+//
+//  Created by Nitin Gupta on 21/04/22.
+//
+
+import Foundation
+
+struct AppConstants {
+    static let borderWidth = 1.0
+    static let cornerRadius = 8.0
+}
